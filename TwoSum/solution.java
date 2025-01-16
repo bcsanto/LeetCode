12db792802934befa1efeb12f8595f6e07a3207b
Brandon C. Santoso <bsanto@uw.edu>
2025-01-16 15:54:43 -0800

    Completed solution, added the file, and included the staic main

---
+++ TwoSum/solution.java
@@ -0,0 +1,25 @@
+/*
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+*/ 
+
+public class solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+    public static void main(String[] args) {
+    solution solution = new solution();
+    int[] nums = {3,2,5,3};
+    int target = 6;
+
+    int[] result = solution.twoSum(nums, target);
+    System.out.println("[" + result[0] + "," + result[1] + "]");
+    }
+}
